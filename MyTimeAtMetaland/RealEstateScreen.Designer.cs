818cef2ae89d86d1b9e49743a9d17af0392c1e66
GorkemKocc <118227992+GorkemKocc@users.noreply.github.com>
2023-05-26 16:49:39 +0300

    s

--- MyTimeAtMetaland/RealEstateScreen.Designer.cs
+++ MyTimeAtMetaland/RealEstateScreen.Designer.cs
@@ -32,16 +32,24 @@
             pictureBox1 = new PictureBox();
             panel1 = new Panel();
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             label2 = new Label();
             pictureBox2 = new PictureBox();
+            label1 = new Label();
+            label3 = new Label();
+            pictureBox3 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Location = new Point(3, 3);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(1000, 555);
             pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
@@ -50,56 +58,134 @@
             // 
             // panel1
             // 
-            panel1.Location = new Point(142, 42);
+            panel1.Location = new Point(144, 44);
             panel1.Name = "panel1";
-            panel1.Size = new Size(858, 379);
+            panel1.Size = new Size(859, 380);
             panel1.TabIndex = 1;
             panel1.Paint += panel1_Paint;
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(908, 3);
+            button1.Location = new Point(916, 6);
             button1.Name = "button1";
-            button1.Size = new Size(79, 36);
+            button1.Size = new Size(76, 32);
             button1.TabIndex = 2;
             button1.Text = "Çıkış";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // button2
+            // 
+            button2.Location = new Point(58, 480);
+            button2.Margin = new Padding(2, 2, 2, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(90, 27);
+            button2.TabIndex = 0;
+            button2.Text = "satın al";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(187, 480);
+            button3.Margin = new Padding(2, 2, 2, 2);
+            button3.Name = "button3";
+            button3.Size = new Size(90, 27);
+            button3.TabIndex = 3;
+            button3.Text = "kirala";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(445, 462);
+            textBox1.Margin = new Padding(2, 2, 2, 2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(168, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(445, 506);
+            textBox2.Margin = new Padding(2, 2, 2, 2);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(168, 27);
+            textBox2.TabIndex = 5;
+            // 
             // label2
             // 
             label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(877, 504);
+            label2.Location = new Point(881, 506);
             label2.Name = "label2";
             label2.Size = new Size(81, 20);
-            label2.TabIndex = 6;
+            label2.TabIndex = 7;
             label2.Text = "100";
             label2.TextAlign = ContentAlignment.MiddleCenter;
+            label2.Click += label2_Click;
             // 
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(891, 449);
+            pictureBox2.Location = new Point(58, 74);
+            pictureBox2.Margin = new Padding(2, 2, 2, 2);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(51, 48);
+            pictureBox2.Size = new Size(48, 48);
             pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
-            pictureBox2.TabIndex = 5;
+            pictureBox2.TabIndex = 1;
             pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(360, 465);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 8;
+            label1.Text = "label1";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(360, 509);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 9;
+            label3.Text = "label3";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(893, 450);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(60, 60);
+            pictureBox3.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox3.TabIndex = 10;
+            pictureBox3.TabStop = false;
             // 
             // RealEstateScreen
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(pictureBox3);
+            Controls.Add(label3);
+            Controls.Add(label1);
             Controls.Add(label2);
-            Controls.Add(button1);
             Controls.Add(pictureBox2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(panel1);
             Controls.Add(pictureBox1);
             Name = "RealEstateScreen";
-            Size = new Size(1000, 555);
+            Size = new Size(1002, 557);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -109,7 +195,14 @@
         private PictureBox pictureBox1;
         private Panel panel1;
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private TextBox textBox2;
         public Label label2;
         public PictureBox pictureBox2;
+        private Label label1;
+        private Label label3;
+        public PictureBox pictureBox3;
     }
 }
